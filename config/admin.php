92152c567f13a96e4665f88833555af9c76ddb99
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-08-05 16:28:17 +0300

    romina

--- config/admin.php
+++ config/admin.php
@@ -95,7 +95,7 @@ return [
     | If your page is going to be accessed via https, set it to `true`.
     |
     */
-    'https' => env('ADMIN_HTTPS', TRUE),
+    'https' => env('ADMIN_HTTPS', FALSE),
 
     /*
     |--------------------------------------------------------------------------
