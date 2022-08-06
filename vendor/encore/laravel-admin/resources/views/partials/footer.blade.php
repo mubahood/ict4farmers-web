9f7e78a22d3cbd1328ce096ad44ea9f338747b8b
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-08-06 18:08:08 +0300

    added help chat

--- vendor/encore/laravel-admin/resources/views/partials/footer.blade.php
+++ vendor/encore/laravel-admin/resources/views/partials/footer.blade.php
@@ -1,4 +1,19 @@
 <!-- Main Footer -->
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API = Tawk_API || {},
+        Tawk_LoadStart = new Date();
+    (function() {
+        var s1 = document.createElement("script"),
+            s0 = document.getElementsByTagName("script")[0];
+        s1.async = true;
+        s1.src = 'https://embed.tawk.to/62ee7a3437898912e961972f/1g9poqoh3';
+        s1.charset = 'UTF-8';
+        s1.setAttribute('crossorigin', '*');
+        s0.parentNode.insertBefore(s1, s0);
+    })();
+</script>
+<!--End of Tawk.to Script-->
 <footer class="main-footer">
     <!-- To the right -->
     <div class="pull-right hidden-xs">
@@ -15,3 +30,4 @@
     </div>
     <strong>Powered by <a href="https://8technologies.net" target="_blank">8Technologies consults</a></strong>
 </footer>
+
