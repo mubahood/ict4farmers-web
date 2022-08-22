aef96c0788166986fac9cca49aef96c3e166e5ac
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-08-22 13:04:23 +0300

    romina

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -367,7 +367,8 @@ thumbnail
         // die("");
         //return view('metro.main.product-listing');
         //return view('metro.main.product-listing');
-        return view('metro.main.index');
+        return view('landing.index');
+        //return view('metro.main.index');
         //return view('metro.index');
     }
 
