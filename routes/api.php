fff80944ef33e739b79a41aa3045cd29a6a71343
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-10-26 05:05:05 +0300

    new update

--- routes/api.php
+++ routes/api.php
@@ -30,7 +30,7 @@ Route::get('wizard-items', [ApiProductsController::class, 'wizard_items']);
 Route::get('app-version', function () {
     return [[
         'id' => 1,
-        'version' => '5',
+        'version' => '5',//
     ]];
 });
 
