58541e1250cd8f8919e4fccab72995d790360079
DALIHILLARY <dalirichardh@gmail.com>
2023-02-08 11:33:40 +0300

    moved cascade to cascade branch

--- app/Admin/Controllers/OrganisationController.php
+++ app/Admin/Controllers/OrganisationController.php
@@ -75,106 +75,13 @@ class OrganisationController extends AdminController
     protected function form()
     {
         $form = new Form(new Organisation());
-        // $form->hasMany('organizations', function (Form\NestedForm $form) {
-            $form->text('name', __('Name'))->required();
-            $form->image('logo', __('Logo'))->rules('mimes:png,jpeg,jpg');
-            $form->text('address', __('Address'))->required();
-            
-            $form->mobile('phone', __('Phone'));
-            $form->email('email', __('Email'))->required();
-            $form->radio('radiocheck')->options([
-                1 => 'NO',
-                2 => 'YES'
-            ])->when(2, function() use($form) {
-                $form->text('website', __('Website'));
-                $form->textarea('details', __('Details'));
-            })->default(1);
- 
-
-            $form->hasMany('organizations',null, function (Form\NestedForm $form) {
-                $form->text('name', __('Name'))->required();
-                $form->image('logo', __('Logo'))->rules('mimes:png,jpeg,jpg');
-                $form->text('address', __('Address'))->required();
-                
-                $form->mobile('phone', __('Phone'));
-                $form->email('email', __('Email'))->required();
-                $form->radio('radiocheck')->options([
-                    1 => 'NO',
-                    2 => 'YES'
-                ])->default(1);
-                    // Admin::script('alert("fellow");');
-                $form->html('<div class="cascade-group cascade-new___LA_KEY__ hide">
-                                <div class="form-group  ">
-                                    <label for="website" class="col-sm-2  control-label">Website</label>
-                                    <div class="col-sm-8">
-                                        <div class="input-group">
-                                            <span class="input-group-addon"><i class="fa fa-pencil fa-fw"></i></span>
-                                            <input type="text" id="website" name="organizations[new___LA_KEY__][website]" value="" class="form-control organizations website" placeholder="Input Website">
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="form-group  ">
-                                    <label for="details" class="col-sm-2  control-label">Details</label>
-                                    <div class="col-sm-8">
-                                        <textarea name="organizations[new___LA_KEY__][details]" class="form-control organizations details" rows="5" placeholder="Input Details"></textarea>
-                                    </div>  
-                                </div>
-                            </div>
-                ');
-
-                $form->html('<script data-exec-on-popstate>
-                var operator_table = {
-                    \'=\': function(a, b) {
-                        if ($.isArray(a) && $.isArray(b)) {
-                            return $(a).not(b).length === 0 && $(b).not(a).length === 0;
-                        }
-            
-                        return a == b;
-                    },
-                    \'>\': function(a, b) { return a > b; },
-                    \'<\': function(a, b) { return a < b; },
-                    \'>=\': function(a, b) { return a >= b; },
-                    \'<=\': function(a, b) { return a <= b; },
-                    \'!=\': function(a, b) {
-                         if ($.isArray(a) && $.isArray(b)) {
-                            return !($(a).not(b).length === 0 && $(b).not(a).length === 0);
-                         }
-            
-                         return a != b;
-                    },
-                    \'in\': function(a, b) { return $.inArray(a, b) != -1; },
-                    \'notIn\': function(a, b) { return $.inArray(a, b) == -1; },
-                    \'has\': function(a, b) { return $.inArray(b, a) != -1; },
-                    \'oneIn\': function(a, b) { return a.filter(v => b.includes(v)).length >= 1; },
-                    \'oneNotIn\': function(a, b) { return a.filter(v => b.includes(v)).length == 0; },
-                };
-                    var cascade_groups = [
-                        { class: "cascade-new___LA_KEY__", operator: "=", value: "2" },
-                    ];
-                
-                    $(\'input[name="organizations[new___LA_KEY__][radiocheck]"]\').on("ifChecked", function (e) {
-                        var checked = $(this).val();
-                        cascade_groups.forEach(function (event) {
-                            console.log(event.class);
-                            var group = $(\'div.cascade-group.\'+event.class);
-                            console.log(operator_table[event.operator](checked, event.value));
-                            if (operator_table[event.operator](checked, event.value)) {
-                                group.removeClass("hide");
-                            } else {
-                                group.addClass("hide");
-                            }
-                        });
-                    });
-                
-                 </script>');
-   
-                //  .cascade-\[new_3\]
-
-
-                });
-        
-        
-
+        $form->text('name', __('Name'))->required();
+        $form->image('logo', __('Logo'))->rules('mimes:png,jpeg,jpg');
+        $form->text('address', __('Address'))->required();
+        $form->mobile('phone', __('Phone'));
+        $form->email('email', __('Email'))->required();
+        $form->text('website', __('Website'));
+        $form->textarea('details', __('Details'));
 
         return $form;
     }
